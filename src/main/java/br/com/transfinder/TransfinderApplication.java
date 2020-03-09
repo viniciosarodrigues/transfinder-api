46c6a576486b900190a7dc4f3e1308b8209a89d4
viniciosarodrigues <viniciosarodrigues@gmail.com>
2020-03-08 21:10:56 -0300

    Criando projeto

---
+++ src/main/java/br/com/transfinder/TransfinderApplication.java
@@ -0,0 +1,13 @@
+package br.com.transfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransfinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/transfinder/TransfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.transfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

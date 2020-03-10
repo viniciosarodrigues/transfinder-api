08f81f2812305e3479df5dd00cdbfbda3aa0c24c
viniciosarodrigues <viniciosarodrigues@gmail.com>
2020-03-09 22:21:42 -0300

    Adicionando objeto de filtragem

---
+++ src/main/java/br/com/transfinder/repository/filter/FilterRequest.java
@@ -0,0 +1,23 @@
+package br.com.transfinder.repository.filter;
+
+import java.io.Serializable;
+
+import br.com.transfinder.model.enums.ModalType;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Data
+@EqualsAndHashCode
+@ToString
+public class FilterRequest implements Serializable {
+	private static final long serialVersionUID = -7571845680560006629L;
+
+	private String name;
+
+	private String uf;
+
+	private String city;
+
+	private ModalType modalType;
+}
